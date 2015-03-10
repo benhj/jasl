84d0ebb6a428ec6780487dc83ad7df81e993036e
benhj <bhj.research@gmail.com>
2015-03-10 12:08:04 -0400

    better build script

--- buildIt.sh
+++ buildIt.sh
@@ -1,35 +1,10 @@
+#!/bin/bash 
 # set build flags
 compiler=clang++
 cflags=-std=c++11
 boostIncPath=/usr/local/include
 
-echo "Building VarCache..."
-$compiler $cflags -I$boostIncPath -c VarCache.cpp -o VarCache.o 
-
-echo "Building CallCommand..."
-$compiler $cflags -I$boostIncPath -c commands/CallCommand.cpp -o CallCommand.o 
-
-echo "Building IfCommand..."
-$compiler $cflags -I$boostIncPath -c commands/IfCommand.cpp -o IfCommand.o 
-
-echo "Building RepeatCommand..."
-$compiler $cflags -I$boostIncPath -c commands/RepeatCommand.cpp -o RepeatCommand.o 
-
-echo "Building StartCommand..."
-$compiler $cflags -I$boostIncPath -c commands/StartCommand.cpp -o StartCommand.o 
-
-echo "Building BlockCommand..."
-$compiler $cflags -I$boostIncPath -c commands/BlockCommand.cpp -o BlockCommand.o 
-
-echo "Building MathExpression..."
-$compiler $cflags -I$boostIncPath -c commands/expressions/MathExpression.cpp -o MathExpression.o 
-
-echo "Building ComparisonExpression..."
-$compiler $cflags -I$boostIncPath -c commands/expressions/ComparisonExpression.cpp -o ComparisonExpression.o 
-
-echo "Building CommandInterpretor..."
-$compiler $cflags -I$boostIncPath -c CommandInterpretor.cpp -o CommandInterpretor.o
-
+# these objects will exist after compilation of everything
 objects="VarCache.o \
          IfCommand.o \
          RepeatCommand.o \
@@ -40,5 +15,70 @@ objects="VarCache.o \
          ComparisonExpression.o \
          CommandInterpretor.o"
 
-echo "Building test..."
-$compiler $cflags -I$boostIncPath $objects test.cpp -o test
+function build_VarCache {
+    echo "Building VarCache..."
+    $compiler $cflags -I$boostIncPath -c VarCache.cpp -o VarCache.o 
+}
+
+function build_CallCommand {
+    echo "Building CallCommand..."
+    $compiler $cflags -I$boostIncPath -c commands/CallCommand.cpp -o CallCommand.o 
+}
+
+function build_IfCommand {
+    echo "Building IfCommand..."
+    $compiler $cflags -I$boostIncPath -c commands/IfCommand.cpp -o IfCommand.o 
+}
+
+function build_RepeatCommand {
+    echo "Building RepeatCommand..."
+    $compiler $cflags -I$boostIncPath -c commands/RepeatCommand.cpp -o RepeatCommand.o 
+}
+
+function build_StartCommand {
+    echo "Building StartCommand..."
+    $compiler $cflags -I$boostIncPath -c commands/StartCommand.cpp -o StartCommand.o 
+}
+
+function build_BlockCommand {
+    echo "Building BlockCommand..."
+    $compiler $cflags -I$boostIncPath -c commands/BlockCommand.cpp -o BlockCommand.o 
+}
+
+function build_MathExpression {
+    echo "Building MathExpression..."
+    $compiler $cflags -I$boostIncPath -c commands/expressions/MathExpression.cpp -o MathExpression.o 
+}
+
+function build_ComparisonExpression {
+    echo "Building ComparisonExpression..."
+    $compiler $cflags -I$boostIncPath -c commands/expressions/ComparisonExpression.cpp -o ComparisonExpression.o 
+}
+
+function build_CommandInterpretor {
+    echo "Building CommandInterpretor..."
+    $compiler $cflags -I$boostIncPath -c CommandInterpretor.cpp -o CommandInterpretor.o
+}
+
+function build_tests {
+    echo "Building tests..."
+    $compiler $cflags -I$boostIncPath $objects test.cpp -o test
+}
+
+function build_all {
+    build_VarCache
+    build_CallCommand
+    build_IfCommand
+    build_RepeatCommand
+    build_StartCommand
+    build_BlockCommand
+    build_MathExpression
+    build_ComparisonExpression
+    build_CommandInterpretor
+    build_tests
+}
+
+# control which parts we need to be built.
+# For example "./sh buildIt.sh all" will build everything
+# while "./sh buildIt.sh tests" will attempt to build the tests
+build_$1
