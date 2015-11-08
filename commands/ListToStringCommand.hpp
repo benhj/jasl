20d4d6edb10ab1695c6e17c3581ddbd798791ef7
Ben Jones <bhj.research@gmail.com>
2015-11-08 16:25:53 +0000

    refactors

--- commands/ListToStringCommand.hpp
+++ commands/ListToStringCommand.hpp
@@ -1,5 +1,5 @@
 //
-//  ListToStringCommand.cpp
+//  ListToStringCommand.hpp
 //  jasl
 //
 //  Created by Ben Jones on 16/03/15
@@ -9,12 +9,6 @@
 #pragma once
 
 #include "Command.hpp"
-#include "../LiteralString.hpp"
-#include "../VarExtractor.hpp"
-#include <boost/lexical_cast.hpp>
-#include <algorithm>
-#include <sstream>
-#include <cstdint>
 
 namespace jasl
 {
@@ -23,92 +17,14 @@ namespace jasl
     public:
         ListToStringCommand(Function &func_,
                             SharedVarCache const &sharedCache = SharedVarCache(),
-                            OptionalOutputStream const &output = OptionalOutputStream())
-        : Command(func_, sharedCache, output)
-        {
-        }
+                            OptionalOutputStream const &output = OptionalOutputStream());
 
-        bool execute() override
-        {
-            std::string varName;
-            if(!m_func.getValueB<std::string>(varName, m_sharedCache)) {
-                setLastErrorMessage("list_to_string: couldn't parse list");
-                return false;
-            }
+        bool execute() override;
 
-            if(tryRawListExtraction(varName)) { return true; }
-            if(trySymbolExtraction(varName)) { return true; }
-
-            return false;
-        }
     private:
-        bool tryRawListExtraction(std::string const &varName) 
-        {
-            ValueArray v;
-            if(m_func.getValueA<ValueArray>(v, m_sharedCache)) {
-                std::string s;
-                try {
-                    for(auto & val : v) {
-                        std::string tok;
-                        if(!VarExtractor::tryAnyCast(tok, val)) {
-                            return false;
-                        }
-                        // tokens separated by a single space
-                        s.append(tok);
-                        s.append(" ");
-                    }
-                    // remove last space
-                    s.pop_back();
-                    
-                    // now finally store string in cache
-                    m_sharedCache->setString(varName, s);
-                    return true;
-                } catch( boost::bad_lexical_cast const& ) {
-                    setLastErrorMessage("list_to_string: couldn't parse list");
-                    return false;
-                }
-            }
-            return false;
-        }
 
-        bool trySymbolExtraction(std::string const &varName)
-        {
-            // Now try extracting a symbol
-            std::string symbol;
-            if(m_func.getValueA<std::string>(symbol, m_sharedCache)) {
-
-                // find the ValueArray in the list cache having symbol symbol
-                auto found = m_sharedCache->getList(symbol);
-
-                // if found then process list
-                if(found) {
-                    std::string s;
-                    try {
-                        for(auto & val : *found) {
-                            std::string tok;
-                            if(!VarExtractor::tryAnyCast(tok, val)) {
-                                return false;
-                            }
-                            // tokens separated by a single space
-                            s.append(tok);
-                            s.append(" ");
-                        }
-                        // remove last space
-                        s.pop_back();
-
-                        // now finally store string in cache
-                        m_sharedCache->setString(varName, s);
-                        return true;
-                    } catch( boost::bad_lexical_cast const& ) {
-                        setLastErrorMessage("list_to_string: couldn't parse list");
-                        return false;
-                    }
-                }
-            }
-           return false;
-        }
-
-        
+        bool tryRawListExtraction(std::string const &varName);
+        bool trySymbolExtraction(std::string const &varName);
     };
 
 }
--- commands/NewPrimitiveSyntaxCommand.hpp
+++ commands/NewPrimitiveSyntaxCommand.hpp
@@ -9,7 +9,6 @@
 #pragma once
 
 #include "Command.hpp"
-#include "StringToPrimitiveCommand.hpp"
 
 namespace jasl {
     class NewPrimitiveSyntaxCommand : public Command
@@ -17,76 +16,13 @@ namespace jasl {
     public:
         NewPrimitiveSyntaxCommand(Function &func_,
                                   SharedVarCache const &sharedCache = SharedVarCache(),
-                                  OptionalOutputStream const &output = OptionalOutputStream())
-        : Command(func_, sharedCache, output)
-        {
-        }
+                                  OptionalOutputStream const &output = OptionalOutputStream());
 
-        bool execute() override
-        {
-
-            std::string type = m_func.name;
-            std::string varName; 
-            (void)m_func.getValueB<std::string>(varName, m_sharedCache);
-
-            if (type == "integer") {
-
-                return handleInt(varName);
-
-            } else if (type == "decimal") {
-
-                return handleDouble(varName);
-
-            } else if (type == "boolean") {
-
-                return handleBool(varName);
-
-            } 
-
-            setLastErrorMessage("cvar: type not supported");
-            return false;
-        }
+        bool execute() override;
 
     private:
-        bool handleInt(std::string const &varName)
-        {
-            auto a = VarExtractor::trySingleIntExtraction(m_func.paramA, m_sharedCache);
-            if (!a) {
-                // try converting a string to an integer
-                return StringToPrimitiveCommand(m_func, m_sharedCache, m_outputStream).execute();
-            } 
-            m_sharedCache->setInt(varName, *a);
-            
-
-            return true;
-        }
-
-        bool handleDouble(std::string const &varName)
-        {
-            auto a = VarExtractor::trySingleDoubleExtraction(m_func.paramA, m_sharedCache);
-            if (!a) {
-                // try converting a string to a double
-                return StringToPrimitiveCommand(m_func, m_sharedCache, m_outputStream).execute();
-            } 
-
-            m_sharedCache->setDouble(varName, *a);
-            
-
-            return true;
-        }
-
-        bool handleBool(std::string const &varName)
-        {
-            auto a = VarExtractor::trySingleBoolExtraction(m_func.paramA, m_sharedCache);
-            if (!a) {
-                return false;
-            } 
-
-            m_sharedCache->setBool(varName, *a);
-            
-
-            return true;
-        }
+        bool handleInt(std::string const &varName);
+        bool handleDouble(std::string const &varName);
+        bool handleBool(std::string const &varName);
     };
 }
-
