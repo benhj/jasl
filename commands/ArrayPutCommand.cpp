d987e9322236e43e66aa4bbafec956881d974df5
benhj <bhj.research@gmail.com>
2016-06-30 15:19:55 +0100

    building in support for arrays

---
+++ commands/ArrayPutCommand.cpp
@@ -0,0 +1,79 @@
+//
+//  ArrayPutCommand.cpp
+//  jasl
+//
+//  Created by Ben Jones on 30/06/15
+//  Copyright (c) 2016 Ben Jones. All rights reserved.
+//
+
+#include "ArrayPutCommand.hpp"
+#include "../VarExtractor.hpp"
+
+namespace jasl
+{
+    ArrayPutCommand::ArrayPutCommand(Function &func_,
+                                     SharedVarCache const &sharedCache,
+                                     OptionalOutputStream const &output)
+    : Command(func_, sharedCache, output)
+    , m_type()
+    , m_varName()
+    {
+        // example
+        // put 5 -> q(0);
+        (void)m_func.getValueB<std::string>(m_varName, m_sharedCache);
+
+        auto type = sharedCache->getType(m_varName);
+        if(!type) {
+            setLastErrorMessage("put: couldn't determine type");
+        } else if(*type == Type::Int) {
+            m_type = "int";
+        } else if(*type == Type::Double) {
+            m_type = "real";
+        } else {
+            setLastErrorMessage("put: couldn't determine type");
+        }
+    }
+
+    bool ArrayPutCommand::execute() 
+    {
+        if(m_type.empty()) {
+            return false;
+        }
+
+        int64_t index;
+        if(!getIndex(index)) {
+            setLastErrorMessage("put: problem extracting index");
+            return false;
+        }
+
+        // see if int or double array
+        if (m_type == "int") {
+            int64_t value;
+            if (!VarExtractor::trySingleIntExtraction(m_func.paramB, value, m_sharedCache)) {
+                // try converting a string to an int
+                setLastErrorMessage("put: problem setting int");
+                return false;
+            } 
+
+            m_sharedCache->setValueInIntArray(m_varName, index, value);
+
+        } else if (m_type == "real") {
+            double value;
+            if (!VarExtractor::trySingleDoubleExtraction(m_func.paramB, value, m_sharedCache)) {
+                // try converting a string to an int
+                setLastErrorMessage("put: problem setting real");
+                return false;
+            } 
+
+            m_sharedCache->setValueInDoubleArray(m_varName, index, value);
+        }
+
+        return true;
+    }
+
+    bool ArrayPutCommand::getIndex(int64_t &index) 
+    {
+        return VarExtractor::trySingleIntExtraction(m_func.paramC, index, m_sharedCache);
+    }
+
+}
---
+++ commands/ArrayPutCommand.hpp
@@ -0,0 +1,29 @@
+//
+//  ArrayPutCommand.hpp
+//  jasl
+//
+//  Created by Ben Jones on 30/06/15
+//  Copyright (c) 2016 Ben Jones. All rights reserved.
+//
+
+#pragma once
+
+#include "Command.hpp"
+
+namespace jasl
+{
+    class ArrayPutCommand : public Command
+    {
+    public:
+        ArrayPutCommand(Function &func_,
+                        SharedVarCache const &sharedCache = SharedVarCache(),
+                        OptionalOutputStream const &output = OptionalOutputStream());
+
+        bool execute() override;
+    private:
+        bool getIndex(int64_t &index);
+        std::string m_type;
+        std::string m_varName;
+    };
+
+}
