f6d939b2008c5b01d90d2509019c7f0b70efbf53
benhj <bhj.research@gmail.com>
2016-07-06 16:02:13 +0100

    extending put to primitives

---
+++ commands/PutPrimitiveCommand.hpp
@@ -0,0 +1,28 @@
+//
+//  PutPrimitiveCommand.hpp
+//  jasl
+//
+//  Created by Ben Jones on 06/07/16
+//  Copyright (c) 2016 Ben Jones. All rights reserved.
+//
+
+#pragma once
+
+#include "Command.hpp"
+
+namespace jasl
+{
+    class PutPrimitiveCommand : public Command
+    {
+    public:
+        PutPrimitiveCommand(Function &func_,
+                            SharedVarCache const &sharedCache = SharedVarCache(),
+                            OptionalOutputStream const &output = OptionalOutputStream());
+
+        bool execute() override;
+    private:
+        std::string m_type;
+        std::string m_varName;
+    };
+
+}
