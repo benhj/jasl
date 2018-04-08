585601e422c000da620718502bbe130e6b8890c2
Ben Jones <bhj.research@gmail.com>
2018-04-08 13:51:28 +0100

    tokenize command

---
+++ src/commands/string/TokenizeCommand.cpp
@@ -0,0 +1,73 @@
+//
+//  TokenizeCommand.cpp
+//  jasl
+//
+//  Copyright (c) 2018-present Ben Jones. All rights reserved.
+//
+
+#include "TokenizeCommand.hpp"
+#include "caching/VarExtractor.hpp"
+#include <sstream>
+
+namespace {
+    jasl::StringArray getStrings(std::string const & str)
+    {
+        std::istringstream iss(str);
+        return jasl::StringArray{std::istream_iterator<std::string>{iss},
+                                 std::istream_iterator<std::string>{}};
+    }
+}
+
+namespace jasl
+{
+    TokenizeCommand::TokenizeCommand(Function &func_,
+                                     SharedCacheStack const &sharedCache,
+                                     OptionalOutputStream const &output)
+    : Command(func_, sharedCache, output)
+    {
+    }
+
+    bool TokenizeCommand::execute() 
+    {
+        std::string listName;
+        if(!m_func.getValueB<std::string>(listName, m_sharedCache)) {
+            setLastErrorMessage("string: couldn't parse");
+            return false;
+        }
+
+        if(tryLiteralExtraction(listName)) { return true; }
+        if(trySymbolExtraction(listName)) { return true; }
+        
+        setLastErrorMessage("string: couldn't parse");
+        return false;
+    }
+
+    bool TokenizeCommand::tryLiteralExtraction(std::string const &key)
+    {
+        LiteralString literalString;
+        if(m_func.getValueA<LiteralString>(literalString, m_sharedCache)) {
+            auto const literal = literalString.literal;
+            auto strs = getStrings(literal);
+            m_sharedCache->setVar(key, strs, Type::List);
+            return true;
+        }
+        return false;
+    }
+
+    bool TokenizeCommand::trySymbolExtraction(std::string const &key)
+    {
+        // Now try extracting a symbol
+        std::string symbol;
+        if(m_func.getValueA<std::string>(symbol, m_sharedCache)) {
+            std::cout<<"A"<<std::endl;
+            std::string value;
+            if( m_sharedCache->getVar_(symbol, value, Type::String)) {
+            std::cout<<"B"<<std::endl;
+                auto strs = getStrings(value);
+                m_sharedCache->setVar(key, strs, Type::List);
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/commands/string/TokenizeCommand.hpp
@@ -0,0 +1,31 @@
+//
+// TokenizeCommand.hpp
+// jasl
+//
+// Copyright (c) 2018-Present Ben Jones. All rights reserved.
+//
+// Tokenizes a string into individual words, storing them
+// in a list
+//
+// tokenize "hello world" -> list;
+
+#pragma once
+
+#include "../Command.hpp"
+
+namespace jasl
+{
+    class TokenizeCommand : public Command
+    {
+      public:
+        TokenizeCommand(Function &func_,
+                        SharedCacheStack const &sharedCache = SharedCacheStack(),
+                        OptionalOutputStream const &output = OptionalOutputStream());
+
+        bool execute() override;
+      private:
+        bool tryLiteralExtraction(std::string const &key);
+        bool trySymbolExtraction(std::string const &key);
+    };
+
+}
