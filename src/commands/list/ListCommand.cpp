fa2372134ed9baa41a5f3b570cef5fb0f029fa27
Ben Jones <bhj.research@gmail.com>
2019-04-20 17:41:47 +0100

    list command with ^insert ala pop-11

--- src/commands/list/ListCommand.cpp
+++ src/commands/list/ListCommand.cpp
@@ -47,13 +47,41 @@ namespace jasl
             }
         }
 
+        // Loop through list, and check if we have any ^element
+        List finalList;
+        for(auto const & el : list) {
+            std::string element;
+            if(VarExtractor::tryAnyCast(element, el)) {
+                if(element.length() > 1 && element[0] == '^') {
+                    std::string const varName(std::begin(element) + 1, std::end(element));
+
+                    // Try and get a string from ^var
+                    auto const val = m_sharedCache->getVar<std::string>(varName, Type::String);
+                    if(val) {
+                        finalList.push_back(*val);
+                    } 
+                    // Try and get a list from ^var
+                    else {
+                        auto const listVal = m_sharedCache->getVar<std::string>(varName, Type::List);
+                        if(listVal) {
+                            finalList.push_back(*listVal);
+                        }
+                    }
+                } else {
+                    finalList.push_back(el);
+                }
+            } else {
+                finalList.push_back(el);
+            }
+        }
+
         std::string listName;
         if(!m_func.getValueB<std::string>(listName, m_sharedCache)) {
             setLastErrorMessage("list: couldn't parse name");
             return false;
         }
 
-        m_sharedCache->setVar(listName, list, Type::List);
+        m_sharedCache->setVar(listName, finalList, Type::List);
         return true;
     }
 }
